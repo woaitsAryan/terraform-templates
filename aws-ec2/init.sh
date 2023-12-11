f5e08726be9a8dd0ab6739ef562b10ce5a805748
woaitsAryan <yoprince.ab@gmail.com>
2023-12-12 01:01:13 +0530

    Fixed aws ec2

---
+++ aws-ec2/init.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+sudo apt-get update
+# Add Docker's official GPG key:
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#Install nginx
+
+sudo apt install nginx
+
+#Install certbot
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+#Write a sample nginx config
+
+sudo cat << EOF > /etc/nginx/sites-available/template
+server {
+    listen 80;
+    server_name yourwebsite.com;
+
+    location / {
+        proxy_pass http://127.0.0.1:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header X-Real-IP \$proxy_protocol_addr;
+        proxy_set_header X-Forwarded-For \$proxy_protocol_addr; 
+    }
+}
+EOF
+
+#Install git
+sudo apt-get install git
+git config --global pull.rebase true
---
+++ aws-ec2/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "ssh-key"{
+    key_name = "ssh-key"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "allow_traffic" {
+    name        = "allow_traffic"
+    description = "Allow inbound traffic"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.ssh-key.key_name
+  vpc_security_group_ids = [aws_security_group.allow_traffic.id ]
+
+  user_data = file("${path.module}/init.sh")
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 20
+    volume_type = "gp2"
+  }
+  
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
